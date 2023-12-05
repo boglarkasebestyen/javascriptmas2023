4268fb4b62709c1c0358e6c7526e84424ce106f7
Boglárka Sebestyén <sebestyenboglarkacsilla@gmail.com>
2023-12-05 14:02:47 -0800

    Added modified html and css.

--- Day1_CountDownToChristmas.js
+++ Day1_CountDownToChristmas.js
@@ -61,3 +61,74 @@ renderHourCountDown()
 // Stretch goals:
 // - Display hours, minutes, seconds.
 // - Add a countdown for another festival, your birthday, or Christmas 2022.
+
+
+/* HTML:
+
+<html>
+    <head>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <h1>Christmas Countdown 🎄</h1>
+        <div class="container">
+            <p id="countdown-displaydays" class="countdown-displaydays">-</p>
+            <p class="countdown-text">days 'til Christmas!</p>
+        </div>
+        
+        <div class="container">
+            <p id="countdown-displayhours" class="countdown-displayhours">-</p>
+            <p class="countdown-text">hours 'til Christmas!</p>
+        </div>
+        
+        <script src="index.js"></script>
+    </body>
+</html>
+
+CSS:
+
+:root {
+  --wine-red: #C7375F;
+  --bright-red: #D42D2F;
+  --dark-green: #344D2F;
+  --light-green: #77A047;
+  --gold: #FAC57D;
+  --snow: #F0F4F7;
+}
+
+html, body {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    text-align: center;
+    color: var(--dark-green);
+    background-color: var(--snow);
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    background: var(--wine-red);
+    color: var(--snow);
+    padding: 1em 2em;
+    border-radius: 5px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+}
+
+.countdown-displaydays {
+    font-size: 100px;
+    margin: 0;
+}
+
+.container {
+    margin: 5px;
+}
+
+/*
